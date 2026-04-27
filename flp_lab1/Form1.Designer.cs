a90330311195758988bd8ce4824b62dffe57ff4d
Darian925 <kazakovadaria150@yandex.ru>
2026-04-27 15:30:45 +0300

    Добавьте файлы проекта.

---
+++ flp_lab1/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace flp_lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDefault = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabNumbers = new System.Windows.Forms.TabPage();
+            this.tabRecipient = new System.Windows.Forms.TabPage();
+            this.tabContent = new System.Windows.Forms.TabPage();
+
+            // Вкладка "Номера и даты"
+            this.lblOutgoingNumber = new System.Windows.Forms.Label();
+            this.txtOutgoingNumber = new System.Windows.Forms.TextBox();
+            this.lblIncomingNumber = new System.Windows.Forms.Label();
+            this.txtIncomingNumber = new System.Windows.Forms.TextBox();
+            this.lblIncomingDate = new System.Windows.Forms.Label();
+            this.txtIncomingDate = new System.Windows.Forms.TextBox();
+
+            // Вкладка "Получатель"
+            this.lblRecipientOrganization = new System.Windows.Forms.Label();
+            this.txtRecipientOrganization = new System.Windows.Forms.TextBox();
+            this.lblPosition = new System.Windows.Forms.Label();
+            this.txtPosition = new System.Windows.Forms.TextBox();
+            this.lblFullName = new System.Windows.Forms.Label();
+            this.txtFullName = new System.Windows.Forms.TextBox();
+            this.lblShortName = new System.Windows.Forms.Label();
+            this.txtShortName = new System.Windows.Forms.TextBox();
+
+            // Вкладка "Содержание"
+            this.lblSubject = new System.Windows.Forms.Label();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.lblStudentName = new System.Windows.Forms.Label();
+            this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.lblBody = new System.Windows.Forms.Label();
+            this.txtBody = new System.Windows.Forms.TextBox();
+
+            // tabControl1
+            this.tabControl1.Controls.Add(this.tabNumbers);
+            this.tabControl1.Controls.Add(this.tabRecipient);
+            this.tabControl1.Controls.Add(this.tabContent);
+            this.tabControl1.Location = new System.Drawing.Point(12, 50);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(480, 380);
+            this.tabControl1.TabIndex = 0;
+
+            // tabNumbers
+            this.tabNumbers.Text = "Номера и даты";
+            this.tabNumbers.Controls.Add(this.lblOutgoingNumber);
+            this.tabNumbers.Controls.Add(this.txtOutgoingNumber);
+            this.tabNumbers.Controls.Add(this.lblIncomingNumber);
+            this.tabNumbers.Controls.Add(this.txtIncomingNumber);
+            this.tabNumbers.Controls.Add(this.lblIncomingDate);
+            this.tabNumbers.Controls.Add(this.txtIncomingDate);
+
+            int y = 25;
+            this.lblOutgoingNumber.AutoSize = true;
+            this.lblOutgoingNumber.Location = new System.Drawing.Point(30, y);
+            this.lblOutgoingNumber.Size = new System.Drawing.Size(160, 20);
+            this.lblOutgoingNumber.Text = "Исходящий номер:";
+            this.txtOutgoingNumber.Location = new System.Drawing.Point(210, y);
+            this.txtOutgoingNumber.Size = new System.Drawing.Size(230, 22);
+
+            y += 45;
+            this.lblIncomingNumber.AutoSize = true;
+            this.lblIncomingNumber.Location = new System.Drawing.Point(30, y);
+            this.lblIncomingNumber.Size = new System.Drawing.Size(155, 20);
+            this.lblIncomingNumber.Text = "Входящий номер:";
+            this.txtIncomingNumber.Location = new System.Drawing.Point(210, y);
+            this.txtIncomingNumber.Size = new System.Drawing.Size(230, 22);
+
+            y += 45;
+            this.lblIncomingDate.AutoSize = true;
+            this.lblIncomingDate.Location = new System.Drawing.Point(30, y);
+            this.lblIncomingDate.Size = new System.Drawing.Size(165, 20);
+            this.lblIncomingDate.Text = "Дата входящего:";
+            this.txtIncomingDate.Location = new System.Drawing.Point(210, y);
+            this.txtIncomingDate.Size = new System.Drawing.Size(230, 22);
+
+            // tabRecipient
+            this.tabRecipient.Text = "Получатель";
+            this.tabRecipient.Controls.Add(this.lblRecipientOrganization);
+            this.tabRecipient.Controls.Add(this.txtRecipientOrganization);
+            this.tabRecipient.Controls.Add(this.lblPosition);
+            this.tabRecipient.Controls.Add(this.txtPosition);
+            this.tabRecipient.Controls.Add(this.lblFullName);
+            this.tabRecipient.Controls.Add(this.txtFullName);
+            this.tabRecipient.Controls.Add(this.lblShortName);
+            this.tabRecipient.Controls.Add(this.txtShortName);
+
+            y = 25;
+            this.lblRecipientOrganization.AutoSize = true;
+            this.lblRecipientOrganization.Location = new System.Drawing.Point(30, y);
+            this.lblRecipientOrganization.Size = new System.Drawing.Size(175, 20);
+            this.lblRecipientOrganization.Text = "Организация получателя:";
+            this.txtRecipientOrganization.Location = new System.Drawing.Point(220, y);
+            this.txtRecipientOrganization.Size = new System.Drawing.Size(220, 22);
+
+            y += 45;
+            this.lblPosition.AutoSize = true;
+            this.lblPosition.Location = new System.Drawing.Point(30, y);
+            this.lblPosition.Size = new System.Drawing.Size(135, 20);
+            this.lblPosition.Text = "Должность:";
+            this.txtPosition.Location = new System.Drawing.Point(220, y);
+            this.txtPosition.Size = new System.Drawing.Size(220, 22);
+
+            y += 45;
+            this.lblFullName.AutoSize = true;
+            this.lblFullName.Location = new System.Drawing.Point(30, y);
+            this.lblFullName.Size = new System.Drawing.Size(135, 20);
+            this.lblFullName.Text = "ФИО адресата:";
+            this.txtFullName.Location = new System.Drawing.Point(220, y);
+            this.txtFullName.Size = new System.Drawing.Size(220, 22);
+
+            y += 45;
+            this.lblShortName.AutoSize = true;
+            this.lblShortName.Location = new System.Drawing.Point(30, y);
+            this.lblShortName.Size = new System.Drawing.Size(160, 20);
+            this.lblShortName.Text = "Обращение (по имени):";
+            this.txtShortName.Location = new System.Drawing.Point(220, y);
+            this.txtShortName.Size = new System.Drawing.Size(220, 22);
+
+            // tabContent
+            this.tabContent.Text = "Содержание";
+            this.tabContent.Controls.Add(this.lblSubject);
+            this.tabContent.Controls.Add(this.txtSubject);
+            this.tabContent.Controls.Add(this.lblStudentName);
+            this.tabContent.Controls.Add(this.txtStudentName);
+            this.tabContent.Controls.Add(this.lblBody);
+            this.tabContent.Controls.Add(this.txtBody);
+
+            y = 25;
+            this.lblSubject.AutoSize = true;
+            this.lblSubject.Location = new System.Drawing.Point(30, y);
+            this.lblSubject.Size = new System.Drawing.Size(105, 20);
+            this.lblSubject.Text = "Тема письма:";
+            this.txtSubject.Location = new System.Drawing.Point(160, y);
+            this.txtSubject.Size = new System.Drawing.Size(270, 22);
+
+            y += 50;
+            this.lblStudentName.AutoSize = true;
+            this.lblStudentName.Location = new System.Drawing.Point(30, y);
+            this.lblStudentName.Size = new System.Drawing.Size(115, 20);
+            this.lblStudentName.Text = "ФИО студента:";
+            this.txtStudentName.Location = new System.Drawing.Point(160, y);
+            this.txtStudentName.Size = new System.Drawing.Size(270, 22);
+
+            y += 50;
+            this.lblBody.AutoSize = true;
+            this.lblBody.Location = new System.Drawing.Point(30, y);
+            this.lblBody.Size = new System.Drawing.Size(115, 20);
+            this.lblBody.Text = "Текст письма:";
+            this.txtBody.Location = new System.Drawing.Point(30, y + 30);
+            this.txtBody.Multiline = true;
+            this.txtBody.Size = new System.Drawing.Size(400, 200);
+            this.txtBody.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+
+            // Buttons
+            this.btnGenerate.BackColor = System.Drawing.Color.LightGreen;
+            this.btnGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.btnGenerate.Location = new System.Drawing.Point(12, 440);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(150, 45);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Сгенерировать";
+            this.btnGenerate.UseVisualStyleBackColor = false;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+
+            this.btnClear.Location = new System.Drawing.Point(172, 440);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(150, 45);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Очистить поля";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+
+            this.btnDefault.Location = new System.Drawing.Point(332, 440);
+            this.btnDefault.Name = "btnDefault";
+            this.btnDefault.Size = new System.Drawing.Size(160, 45);
+            this.btnDefault.TabIndex = 3;
+            this.btnDefault.Text = "Заполнить примером";
+            this.btnDefault.UseVisualStyleBackColor = true;
+            this.btnDefault.Click += new System.EventHandler(this.btnDefault_Click);
+
+            // Form1
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(480, 35);
+            this.lblTitle.Text = "Генератор писем DOCX";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 500);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.btnDefault);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Генератор писем DOCX";
+            this.ResumeLayout(false);
+        }
+
+        // Объявления всех контролов
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDefault;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabNumbers;
+        private System.Windows.Forms.TabPage tabRecipient;
+        private System.Windows.Forms.TabPage tabContent;
+
+        // Номера и даты
+        private System.Windows.Forms.Label lblOutgoingNumber;
+        private System.Windows.Forms.TextBox txtOutgoingNumber;
+        private System.Windows.Forms.Label lblIncomingNumber;
+        private System.Windows.Forms.TextBox txtIncomingNumber;
+        private System.Windows.Forms.Label lblIncomingDate;
+        private System.Windows.Forms.TextBox txtIncomingDate;
+
+        // Получатель
+        private System.Windows.Forms.Label lblRecipientOrganization;
+        private System.Windows.Forms.TextBox txtRecipientOrganization;
+        private System.Windows.Forms.Label lblPosition;
+        private System.Windows.Forms.TextBox txtPosition;
+        private System.Windows.Forms.Label lblFullName;
+        private System.Windows.Forms.TextBox txtFullName;
+        private System.Windows.Forms.Label lblShortName;
+        private System.Windows.Forms.TextBox txtShortName;
+
+        // Содержание
+        private System.Windows.Forms.Label lblSubject;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.Label lblStudentName;
+        private System.Windows.Forms.TextBox txtStudentName;
+        private System.Windows.Forms.Label lblBody;
+        private System.Windows.Forms.TextBox txtBody;
+    }
+}
+
---
+++ flp_lab1/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Windows.Forms;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using DocumentFormat.OpenXml;
+
+namespace flp_lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            // Проверка заполнения полей
+            if (string.IsNullOrWhiteSpace(txtOutgoingNumber.Text) ||
+                string.IsNullOrWhiteSpace(txtIncomingNumber.Text) ||
+                string.IsNullOrWhiteSpace(txtIncomingDate.Text) ||
+                string.IsNullOrWhiteSpace(txtRecipientOrganization.Text) ||
+                string.IsNullOrWhiteSpace(txtPosition.Text) ||
+                string.IsNullOrWhiteSpace(txtFullName.Text) ||
+                string.IsNullOrWhiteSpace(txtShortName.Text) ||
+                string.IsNullOrWhiteSpace(txtSubject.Text) ||
+                string.IsNullOrWhiteSpace(txtStudentName.Text) ||
+                string.IsNullOrWhiteSpace(txtBody.Text))
+            {
+                MessageBox.Show("Пожалуйста, заполните все поля.", "Внимание",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string templatePath = Path.Combine(Application.StartupPath, "Template.docx");
+
+            if (!File.Exists(templatePath))
+            {
+                templatePath = @"C:\Template.docx";
+            }
+
+            if (!File.Exists(templatePath))
+            {
+                MessageBox.Show("Файл шаблона не найден!\n" +
+                    "Поместите Template.docx в папку с программой или в C:\\Template.docx",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            saveFileDialog1.Filter = "Word Document|*.docx";
+            saveFileDialog1.FileName = $"Письмо_{DateTime.Now:yyyyMMdd_HHmmss}.docx";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string outputPath = saveFileDialog1.FileName;
+
+                try
+                {
+                    File.Copy(templatePath, outputPath, true);
+
+                    using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(outputPath, true))
+                    {
+                        ReplacePlaceholdersImproved(wordDoc);
+                        wordDoc.MainDocumentPart.Document.Save();
+                    }
+
+                    MessageBox.Show($"Документ успешно создан!\n{outputPath}", "Готово",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    if (MessageBox.Show("Открыть созданный документ?", "Просмотр",
+                        MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        System.Diagnostics.Process.Start(outputPath);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при генерации документа:\n{ex.Message}",
+                        "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+      
+        private void ReplacePlaceholdersImproved(WordprocessingDocument wordDoc)
+        {
+            var replacements = GetReplacementsDictionary();
+
+            
+            ReplaceTextInBody(wordDoc.MainDocumentPart.Document.Body, replacements);
+
+           
+            foreach (var headerPart in wordDoc.MainDocumentPart.HeaderParts)
+            {
+                if (headerPart.Header != null)
+                    ReplaceTextInBody(headerPart.Header, replacements);
+            }
+
+            foreach (var footerPart in wordDoc.MainDocumentPart.FooterParts)
+            {
+                if (footerPart.Footer != null)
+                    ReplaceTextInBody(footerPart.Footer, replacements);
+            }
+        }
+
+        
+        private void ReplaceTextInBody(OpenXmlElement element, Dictionary<string, string> replacements)
+        {
+            
+            var paragraphs = element.Descendants<Paragraph>().ToList();
+
+            foreach (var paragraph in paragraphs)
+            {
+                
+                var textElements = paragraph.Descendants<Text>().ToList();
+
+                if (textElements.Count == 0) continue;
+
+                
+                string fullText = string.Concat(textElements.Select(t => t.Text));
+
+                
+                bool hasPlaceholder = false;
+                foreach (var placeholder in replacements.Keys)
+                {
+                    if (fullText.Contains(placeholder))
+                    {
+                        hasPlaceholder = true;
+                        break;
+                    }
+                }
+
+                if (!hasPlaceholder) continue;
+
+                
+                string newText = fullText;
+                foreach (var placeholder in replacements)
+                {
+                    newText = newText.Replace(placeholder.Key, placeholder.Value);
+                }
+
+                
+                paragraph.RemoveAllChildren<Run>();
+
+                var newRun = new Run();
+                var newTextElement = new Text(newText);
+
+                
+                if (textElements.First().Parent is Run firstRun)
+                {
+                    
+                    var runProperties = firstRun.RunProperties?.Clone();
+                    if (runProperties != null)
+                        newRun.RunProperties = (RunProperties)runProperties;
+                }
+
+                newRun.Append(newTextElement);
+                paragraph.Append(newRun);
+            }
+        }
+
+        private Dictionary<string, string> GetReplacementsDictionary()
+        {
+            return new Dictionary<string, string>
+            {
+                { "{CURRENT_DATE}", DateTime.Now.ToString("dd.MM.yyyy") },
+                { "{OUTGOING_NUMBER}", txtOutgoingNumber.Text },
+                { "{INCOMING_NUMBER}", txtIncomingNumber.Text },
+                { "{INCOMING_DATE}", txtIncomingDate.Text },
+                { "{RECIPIENT_ORGANIZATION}", txtRecipientOrganization.Text },
+                { "{RECIPIENT_POSITION}", txtPosition.Text },
+                { "{RECIPIENT_NAME}", txtFullName.Text },
+                { "{RECIPIENT_NAME_SHORT}", txtShortName.Text },
+                { "{LETTER_SUBJECT}", txtSubject.Text },
+                { "{RECIPIENT_BODY}", txtBody.Text },
+                { "{STUDENT_NAME}", txtStudentName.Text }
+            };
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtOutgoingNumber.Clear();
+            txtIncomingNumber.Clear();
+            txtIncomingDate.Clear();
+            txtRecipientOrganization.Clear();
+            txtPosition.Clear();
+            txtFullName.Clear();
+            txtShortName.Clear();
+            txtSubject.Clear();
+            txtStudentName.Clear();
+            txtBody.Clear();
+        }
+
+        private void btnDefault_Click(object sender, EventArgs e)
+        {
+            txtOutgoingNumber.Text = "15/05";
+            txtIncomingNumber.Text = "28/04";
+            txtIncomingDate.Text = "20.04.2026";
+            txtRecipientOrganization.Text = "ООО \"Станкин\"";
+            txtPosition.Text = "Старосте";
+            txtFullName.Text = "Петрову Петру Викторовичу";
+            txtShortName.Text = "Пётр Викторович";
+            txtSubject.Text = "О сдаче лабораторных работ";
+            txtStudentName.Text = "Иванов Роман Валерьевич";
+            txtBody.Text = "Прошу разрешить мне сдать лабораторные работы. Все задания выполнены в полном объеме и готовы к защите.";
+        }
+    }
+}
